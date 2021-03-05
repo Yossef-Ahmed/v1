0f18f82c6470a58d130b63e8b75e5045c973ac4d
Youssef Ahmed <uosefd@gmail.com>
2021-03-05 23:23:50 +0200

    Remove The Navbar Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,28 +7,6 @@ export class Navbar extends Component {
     render() {
         return (
             <nav className="navbar container">
-                <div className="hamburger-icon" onClick={this.toggleHamburgerMenu}>
-                    <i className="fas fa-bars"></i>
-                </div>
-                <div className="hamburger-menu">
-                    <div className="hamburger-menu__close-btn" onClick={this.toggleHamburgerMenu}>
-                        <i className="fas fa-times"></i>
-                    </div>
-                    <ul className="hamburger-menu__links-list">
-                        <li className="hamburger-menu__link">
-                            <a href="#">About</a>
-                        </li>
-                        <li className="hamburger-menu__link">
-                            <a href="#">My Skills</a>
-                        </li>
-                        <li className="hamburger-menu__link">
-                            <a href="#">My Resume</a>
-                        </li>
-                        <li className="hamburger-menu__link">
-                            <a href="#">Featured Projects</a>
-                        </li>
-                    </ul>
-                </div>
                 <div className="switch-wrapper">
                     <i className="fas fa-sun"></i>
                     <div className="toggle-wrapper" onClick={this.props.toggleDarkTheme}>
