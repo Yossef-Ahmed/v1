12615a35f0a15cc75fdca45e92eb0f7c511e665d
Youssef Ahmed <uosefd@gmail.com>
2021-09-08 15:19:56 +0200

    Update FeaturedProjects.js

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import ExpenseTrackerImage from "../images/projects/Expense Tracker.png";
+import WeelzImage from "../images/projects/Weelz.jpeg";
+
 const ExpenseTrackerLink = "https://track-my-money.herokuapp.com/";
+const WeelzLink = "https://github.com/Yossef-Ahmed/web-app/tree/csb-9g327";
 
 export class FeaturedProjects extends Component {
     render() {
@@ -17,6 +20,18 @@ export class FeaturedProjects extends Component {
                                 <a href={ExpenseTrackerLink}>Expense Tracker</a>
                             </h3>
                             <p className="featured-project__description">A finance management tool build with the MERN stack to help you record your daily transactions easily, discover your spending habits, and in general, Discover where your money goes.</p>
+                        </div>
+                    </div>
+
+                    <div className="featured-project">
+                        <a href={WeelzLink} className="featured-project__pic" target="_blank">
+                            <img src={WeelzImage} alt="Featured Project Image"/>
+                        </a>
+                        <div className="featured-project__caption">
+                            <h3 className="featured-project__name">
+                                <a href={WeelzLink}>Weelz</a>
+                            </h3>
+                            <p className="featured-project__description">A vehicles app for transportation built with the MERN stack to help travelers travel between cities easily.</p>
                         </div>
                     </div>
                 </div>
